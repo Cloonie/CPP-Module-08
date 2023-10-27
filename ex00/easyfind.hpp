bfbc33bfd0e7c6404895907586fec7b8c0c7fcea
root <rsdoot@DESKTOP-T5CUUII>
2023-10-27 23:45:01 +0800

    redo ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,12 +1,24 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 template <typename T>
-int	easyfind(const T& container, int value)
+typename T::iterator	easyfind(T& container, int value)
 {
-	for (int i = 0; container[i]; i++)
-		if (container[i] == value)
-			return i;
-	throw std::runtime_error("Element not found in the container");
+	typename T::iterator element = std::find(container.begin(), container.end(), value);
+	if (element != container.end())
+		return element;
+	else
+		throw std::runtime_error("Element not found in the container");
 }
+
+// template <typename T>
+// int	easyfind(const T& container, int value)
+// {
+// 	for (int i = 0; container[i]; i++)
+// 		if (container[i] == value)
+// 			return i;
+// 	throw std::runtime_error("Element not found in the container");
+// }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,23 +1,23 @@
 #include "easyfind.hpp"
 
-#include <vector>
-
 int	main(void)
 {
 	std::vector<int> vec;
-	vec.push_back(1); // index: 0
+	vec.push_back(1);
 	vec.push_back(4);
 	vec.push_back(7);
-	vec.push_back(10);
-	vec.push_back(99); // index: 4
+	vec.push_back(8);
+	vec.push_back(9);
 
 	try
 	{
-		int index1 = easyfind(vec, 99);
-		std::cout << "Found at index: " << index1 << std::endl;
+		int target = 4;
+		std::vector<int>::iterator index = easyfind(vec, target);
+		std::cout << "Found value: " << *index << std::endl;
 
-		int index2 = easyfind(vec, 2);  // Element not in the vector
-		std::cout << "Found at index: " << index2 << std::endl;
+		target = 6;
+		index = easyfind(vec, target);
+		std::cout << "Found value: " << *index << std::endl;
 	}
 	catch (const std::runtime_error& e)
 	{
